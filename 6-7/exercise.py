8944fe19dbb70f4779be491432e5e6a6cee70bf7
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-07-06 20:00:11 +0300

    message

--- 6-7/exercise.py
+++ 6-7/exercise.py
@@ -7,7 +7,8 @@ res = requests.get(url) # fire http get request to json api
 users = res.json()# convert the data to a list of dict
 
 print(users)
-file =open('email.txt','w')
-for user in users:
+file =open('email.txt','w') # open new email file or override it if its exists
+for user in users: # iterate all the users and enter the new email to the file
     file.write(user['name'].replace(' ','')+'@'+user['address']['city'].replace(' ' ,'')+'.com\n')
+    #replace all spaces with nothing and build an email with the username
 
